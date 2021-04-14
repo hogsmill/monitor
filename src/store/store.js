39f5aeaa63c2ddff88da3868a943c4d8b200822e
hogsmill <info@hogsmill.com>
2021-04-14 09:48:36 +0100

    add server

--- src/store/store.js
+++ src/store/store.js
@@ -63,8 +63,9 @@ export const store = new Vuex.Store({
       })
       for (let j = 0; j < state.processes.length; j++) {
         const server = state.processes[j].server
+        console.log(state.processes[j], server)
         if (!state.servers[server]) {
-          state.servers[server] = { ok: true }
+          state.servers[server] = { name: server, ok: true }
         }
       }
     },
