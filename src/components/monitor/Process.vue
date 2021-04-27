33cfa5b07cf76f5aa537fc191b73af0100af3cf5
hogsmill <info@hogsmill.com>
2021-04-27 15:36:45 +0100

    deploy

--- src/components/monitor/Process.vue
+++ src/components/monitor/Process.vue
@@ -12,7 +12,9 @@
     </thead>
     <tbody>
       <tr v-for="(app, index) in processes" :key="index">
-        <td v-if="app.server == selectedServer" :class="status(app)">{{ app.newDeploy }}</td>
+        <td v-if="app.server == selectedServer" :class="status(app)">
+          <input type="checkbox" :checked="app.newDeploy">
+        </td>
         <td v-if="app.server == selectedServer" :class="status(app)">{{ app.port }}</td>
         <td v-if="app.server == selectedServer" :class="status(app)" class="app"><a :href="'https://agilesimulations.co.uk/' + app.app" target="blank">{{ app.name }}</a></td>
         <td v-if="app.server == selectedServer" :class="status(app)">
