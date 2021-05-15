ebb86ff7ff88225be9410faceda4b05e00b9b1d7
hogsmill <info@hogsmill.com>
2021-05-15 17:16:30 +0100

    layout

--- src/components/monitor/Process.vue
+++ src/components/monitor/Process.vue
@@ -75,8 +75,8 @@ export default {
   .apps {
     td {
      padding: 0;
-     border-top: 1px solid;
-     border-bottom: 1px solid;
+     border-top: 1px solid #fff;
+     border-bottom: 1px solid #fff;
     }
   }
   .app {
