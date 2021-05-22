c61b52057394816a0d2e5e3ee3e0547a57df49b0
hogsmill <info@hogsmill.com>
2021-05-22 20:23:30 +0100

    use wanted v current

--- src/lib/outdated.php
+++ src/lib/outdated.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
 
-$ignore = array(
-  'sass-loader',
-  'node-sass',
-  'chart.js'
-);
+#$ignore = array(
+#  'sass-loader',
+#  'node-sass',
+#  'chart.js'
+#);
 
 $root = '/usr/apps/';
 $dirs = scandir($root);
@@ -18,7 +18,11 @@ foreach ($dirs as $d) {
     $outdated[$d] = array();
     for ($i = 1; $i < count($output); $i++) {
       $fields = preg_split("/[\s]+/", $output[$i]);
-      if ($fields[0] != '' && !in_array($fields[0], $ignore)) {
+      # if ($fields[0] != '' && !in_array($fields[0], $ignore)) {
+      $package = $fields[0]
+      $current = $fields[1]
+      $wanted = $fields[2]
+      if ($package != '' && $current != $wanted) {
         array_push($outdated[$d], $fields[0]);
       }
     }
